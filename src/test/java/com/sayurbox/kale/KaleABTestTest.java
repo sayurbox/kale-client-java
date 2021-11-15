f44d5cba2f4bd70f41a9b3228dcaf929864e9749
clchinara <audrey.chinara@gmail.com>
2021-11-15 10:56:05 +0700

    Update test

--- src/test/java/com/sayurbox/kale/KaleABTestTest.java
+++ src/test/java/com/sayurbox/kale/KaleABTestTest.java
@@ -1,11 +1,12 @@
 package com.sayurbox.kale;
 
-import com.google.gson.JsonArray;
+import com.sayurbox.kale.abtest.client.GetUniverseAllocationResponse;
 import com.sayurbox.kale.config.KaleConfig;
 
-import com.google.gson.JsonObject;
 import com.sayurbox.kale.abtest.ABTestClient;
 import com.sayurbox.kale.abtest.ABTestClientImpl;
+
+import java.util.List;
 
 public class KaleABTestTest {
 
@@ -19,10 +20,31 @@ public class KaleABTestTest {
                 .withHystrixHealthSnapshotInterval(500) // optional
                 .build();
         ABTestClient abTest = new ABTestClientImpl(config);
-    
-        JsonObject universeAllocation = abTest.getUniverseAllocation("qFA88l70U4RxscuJZFUwEZpHUUUF", "0b95519c-1b32-47fe-aa8c-55cb65d6f8c4");
-        JsonArray allUniverseAllocations = abTest.getAllUniverseAllocations("qFA88l70U4RxscuJZFUwEZpHUUUF");
+
+        GetUniverseAllocationResponse universeAllocation = abTest.getUniverseAllocation(
+                "qFA88l70U4RxscuJZFUwEZpHUUUF", "0b95519c-1b32-47fe-aa8c-55cb65d6f8c4"
+        );
+        List<GetUniverseAllocationResponse> allUniverseAllocations = abTest.getAllUniverseAllocations(
+                "qFA88l70U4RxscuJZFUwEZpHUUUF"
+        );
         System.out.println(universeAllocation);
+        System.out.printf("Universe: %s Experiment: %s Variant: %s Configs: %s User: %s\n",
+                universeAllocation.getUniverseId(),
+                universeAllocation.getExperimentId(),
+                universeAllocation.getVariantId(),
+                universeAllocation.getConfigs(),
+                universeAllocation.getUserId()
+        );
+
         System.out.println(allUniverseAllocations);
+        for (GetUniverseAllocationResponse allocation : allUniverseAllocations) {
+            System.out.printf("\nUniverse: %s Experiment: %s Variant: %s Configs: %s User: %s",
+                    allocation.getUniverseId(),
+                    allocation.getExperimentId(),
+                    allocation.getVariantId(),
+                    allocation.getConfigs(),
+                    allocation.getUserId()
+            );
+        }
     }
 }
