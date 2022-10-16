7b59139ee78e28bb6ddd8fd734510675364fd682
andrisasuke <andri.sasuki@gmail.com>
2022-10-16 19:22:59 +0700

    pip install

--- scripts/pr_checker.py
+++ scripts/pr_checker.py
@@ -4,8 +4,7 @@ import requests
 
 
 def check_pull_request(sonar_token: str, project: str, pr_id: str):
-    sonar_token_bytes = f"{sonar_token}:".encode("ascii")
-    sonar_token_base64_bytes = base64.b64encode(sonar_token_bytes)
+    sonar_token_base64_bytes = base64.b64encode(f"{sonar_token}:".encode("ascii"))
     sonar_token_base64_string = sonar_token_base64_bytes.decode("ascii")
     url = f"https://sonarcloud.io/api/project_pull_requests/list?project={project}"
     headers = {
@@ -22,8 +21,13 @@ def check_pull_request(sonar_token: str, project: str, pr_id: str):
         return
     pr = list[0]
     print(f'{project} pr_number #{pr_id} status', pr['status'])
+    
+    if pr["status"]["bugs"] > 0:
+        raise Exception("Bugs violation")
+    
     if pr["status"]["codeSmells"] > 0:
         raise Exception("Code Smells violation")
+    
     print("Success")
 
 if __name__ == "__main__":
